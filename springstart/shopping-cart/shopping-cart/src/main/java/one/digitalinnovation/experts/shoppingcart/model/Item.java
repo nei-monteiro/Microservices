d023f4c0fda05f1530ccbc75f8156c8fe72927ea
nei-monteiro <nei@nei.eng.br>
2021-08-29 23:57:52 -0300

    Initial commit

---
+++ springstart/shopping-cart/shopping-cart/src/main/java/one/digitalinnovation/experts/shoppingcart/model/Item.java
@@ -0,0 +1,19 @@
+package one.digitalinnovation.experts.shoppingcart.model;
+
+import org.springframework.data.redis.core.RedisHash;
+
+@RedisHash("Item")
+public class Item {
+
+    private Integer productId;
+    private Integer amount;
+
+    public Integer getProductId() { return productId;}
+
+    public void setProductId(Integer productId) {this.productId = productId;}
+
+    public Integer getAmount() { return amount; }
+
+    public void setAmount(Integer amount) {this.amount = amount;}
+
+}
